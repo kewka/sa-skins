d066dfebc3ab3b09f771bedcd38a2674562da9ab
Nikita Korshunov <kewka96@gmail.com>
2019-10-19 21:16:06 +0500

    Configure after init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import SkinPage from 'pages/SkinPage';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Route path="/skins/:id(\d+)" component={SkinPage} />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/SkinPage/SkinPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SkinPage: React.FC = () => <div>SkinPage</div>;
+
+export default SkinPage;
