6622685807fb9a5de94ec429ac078aa03128e540
Nikita Korshunov <kewka96@gmail.com>
2019-10-21 23:31:29 +0500

    Updates

---
+++ precache-manifest.57c8624773a178e15bacd687f422bc4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5423ce1b6aa8cd0060739c6bd2427749",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1a7c54fb153df0195d",
+    "url": "/static/css/2.e9a6d725.chunk.css"
+  },
+  {
+    "revision": "d8ba9beff2998a2016ff",
+    "url": "/static/css/main.648da8ce.chunk.css"
+  },
+  {
+    "revision": "af1a7c54fb153df0195d",
+    "url": "/static/js/2.4db23e3f.chunk.js"
+  },
+  {
+    "revision": "d8ba9beff2998a2016ff",
+    "url": "/static/js/main.e31ab91c.chunk.js"
+  },
+  {
+    "revision": "47c5a43a096b693d6fc2",
+    "url": "/static/js/runtime-main.48b6a681.js"
+  }
+]);
